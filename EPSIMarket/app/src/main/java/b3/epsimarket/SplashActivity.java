72b25667078e32aa59e945f0ecbb7242e19f981b
Anaël <anaelakue60@gmail.com>
2020-03-31 23:30:24 +0200

    Ajout du code pour la réalisation du SplashScreen dans SplashActivity

--- EPSIMarket/app/src/main/java/b3/epsimarket/SplashActivity.java
+++ EPSIMarket/app/src/main/java/b3/epsimarket/SplashActivity.java
@@ -1,7 +1,28 @@
 package b3.epsimarket;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_home);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
 }
