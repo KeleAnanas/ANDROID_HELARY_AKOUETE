ec32ba66e24f4b492070f333c004d4b80ffbbee6
Anaël <anaelakue60@gmail.com>
2020-04-01 13:29:47 +0200

    Ajout de commentaire dans SplashActivity

--- EPSIMarket/app/src/main/java/b3/epsimarket/SplashActivity.java
+++ EPSIMarket/app/src/main/java/b3/epsimarket/SplashActivity.java
@@ -8,8 +8,11 @@ import android.os.PersistableBundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Création de l'activité pour l'affichage du splashscreen
+ */
 public class SplashActivity extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    private final int SPLASH_DISPLAY_LENGTH = 2000; //Durée d'affichage du splashscreen en ms
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
