b6f199fe648dd54696c713abc62d9df9019e6662
Anaël <anaelakue60@gmail.com>
2020-04-02 20:42:36 +0200

    Ajout de commentaire dans SplashActivity sur le timer

--- EPSIMarket/app/src/main/java/b3/epsimarket/SplashActivity.java
+++ EPSIMarket/app/src/main/java/b3/epsimarket/SplashActivity.java
@@ -12,18 +12,27 @@ import androidx.appcompat.app.AppCompatActivity;
  * Création de l'activité pour l'affichage du splashscreen
  */
 public class SplashActivity extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 2000; //Durée d'affichage du splashscreen en ms
+
+    //Durée d'affichage du splashscreen en ms
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        /**
+         * Affichage du splashscreen avec un timer
+         */
         new Handler().postDelayed(new Runnable() {
+
+            //Cette méthode est executé une fois que le timer est fini
             @Override
             public void run() {
                 Intent i = new Intent(SplashActivity.this, HomeActivity.class);
                 startActivity(i);
+
+                //Fermeture de l'activité
                 finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
