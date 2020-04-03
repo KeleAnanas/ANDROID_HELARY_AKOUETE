4101518b737558c60a046b71e40ce2730a9511d4
Anaël <anaelakue60@gmail.com>
2020-04-03 19:14:41 +0200

    Ajout du back button dans GroupinfosActivity

--- EPSIMarket/app/src/main/java/b3/epsimarket/GroupInfosActivity.java
+++ EPSIMarket/app/src/main/java/b3/epsimarket/GroupInfosActivity.java
@@ -2,9 +2,12 @@ package b3.epsimarket;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class GroupInfosActivity extends AppCompatActivity {
 
@@ -12,5 +15,12 @@ public class GroupInfosActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_infos);
+
+        //Déclaration de la toolbar dans l'activité
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_groupinfo_toolbar);
+        setSupportActionBar(toolbar);
+        //Affichage du bouton back
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
--- EPSIMarket/app/src/main/java/b3/epsimarket/HomeActivity.java
+++ EPSIMarket/app/src/main/java/b3/epsimarket/HomeActivity.java
@@ -1,12 +1,17 @@
 package b3.epsimarket;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * Activité de la page principale de l'application
+ */
 public class HomeActivity extends AppCompatActivity {
     private Button btnZone1;
     private Button btnZone2;
@@ -19,6 +24,7 @@ public class HomeActivity extends AppCompatActivity {
         btnZone1 = (Button) findViewById(R.id.activity_home_btn1);
         btnZone2 = (Button) findViewById(R.id.activity_home_btn2);
 
+        //Lancement de l'activité GroupInfo lorsque le bouton est appuyé
         btnZone1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
